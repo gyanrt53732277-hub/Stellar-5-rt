c1dcb276c861c69380a4fea29c7927e5d6aed7db
GP <gyanrt53732277@gmail.com>
2026-08-22 19:38:13 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */ 
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@creit.tech/stellar-wallets-kit', '@stellar/freighter-api', 'framer-motion'],
